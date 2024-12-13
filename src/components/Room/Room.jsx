a544238d932daf7042c1b7ea863d7ec295a29925
karma2912 <rajakyash23@gmail.com>
2024-12-14 02:21:14 +0530

    commit

--- src/components/Room/Room.jsx
+++ src/components/Room/Room.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import PhaserGame from '../../PhaserGame'
-import image from './image1.png'
 
 const Room = () => {
   return (
     <>
     
-    <PhaserGame image={image}/>
+    <PhaserGame />
     </>
   )
 }
